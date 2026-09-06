37a7dbf5f8f216896c45ad2fdba0c721e63e0573
Omiiii <omghotekar01@gmail.com>
2026-09-07 01:17:21 +0530

    Load post explorer styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import ConnectionCenter from './ConnectionCenter';
 import FreeConnectorPanel from './FreeConnectorPanel';
 import './styles.css';
+import './post-explorer.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
