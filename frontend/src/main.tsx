448f00744d0cd662bdf62b183e1787c2d2c8cee3
Omiiii <omghotekar01@gmail.com>
2026-09-07 01:00:57 +0530

    Wire Connection Center into analyst console

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ConnectionCenter from './ConnectionCenter';
 import FreeConnectorPanel from './FreeConnectorPanel';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ConnectionCenter />
     <FreeConnectorPanel />
     <App />
   </React.StrictMode>,
