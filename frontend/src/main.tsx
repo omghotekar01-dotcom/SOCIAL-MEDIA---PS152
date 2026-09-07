63d43d13cec8a660c6047412b784d6cd48a36c1b
Omiiii <omghotekar01@gmail.com>
2026-09-07 19:17:07 +0530

    Wire premium theme and utility drawers into frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ConnectionCenter from './ConnectionCenter';
 import FreeConnectorPanel from './FreeConnectorPanel';
+import ThemeController from './ThemeController';
 import './styles.css';
 import './post-explorer.css';
+import './premium-ui.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeController />
+    <App />
     <ConnectionCenter />
     <FreeConnectorPanel />
-    <App />
   </React.StrictMode>,
 );
