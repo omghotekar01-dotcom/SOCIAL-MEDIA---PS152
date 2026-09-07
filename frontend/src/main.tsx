c4cf0cc28b29fa092470134812260c32e0877fbf
Omiiii <omghotekar01@gmail.com>
2026-09-07 23:12:55 +0530

    feat(ui): boot intelligence command center

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import IntelligenceApp from './IntelligenceApp';
+import './intelligence.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <IntelligenceApp />
   </React.StrictMode>,
 );
